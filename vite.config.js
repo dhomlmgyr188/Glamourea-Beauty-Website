37f4678e03cbdb83c7a58666a3668375f4252475
dhomlmgyr <dhomlmgyr@gmail.com>
2026-04-27 06:03:57 +0300

    تهيئة ملفات المشروع للمرة الأولى

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-const path = require('path')
+const path = require("path");
+import handlebars from "vite-plugin-handlebars";
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, "src"),
+  plugins: [
+    handlebars({
+      partialDirectory: "./src/partials",
+    }),
+  ],
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
   },
   server: {
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+};
