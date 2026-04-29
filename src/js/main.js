0a03b550d0b8869847fca4bbf232a1e714958ad4
dhomlmgyr <dhomlmgyr@gmail.com>
2026-04-29 07:49:46 +0300

    مكون هيرو

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,38 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+const navbar = document.querySelector('.c-navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('is-scrolled');
+  } else {
+    navbar.classList.remove('is-scrolled');
+  }
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".c-navbar__links a");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const id = entry.target.id;
+
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${id}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.6,
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
